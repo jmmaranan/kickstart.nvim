2896866412c299a3a1132722868e30ecd6ec77b9
jmmaranan <jmmaranan@gmail.com>
2023-04-06 22:19:33 -0400

    Update options and keybindings

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -3,6 +3,14 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.autoindent = true
 vim.opt.expandtab = true
+vim.opt.cursorline = true
 
-vim.keymap.set('n', '<leader>nn', ':NeoTreeShowToggle<cr>', {desc = '[N]eoTree[T]oggle'})
+vim.keymap.set('n', '<leader>nf', ':NeoTreeFocusToggle<cr>', {desc = '[N]eoTree[T]oggle'})
 vim.keymap.set('n', '<leader>ft', ':Format<cr>', {desc = '[F]orma[t]'})
+vim.keymap.set('n', '<leader>h', ':wincmd h<cr>',{desc = 'Focus Window Left [H]'})
+vim.keymap.set('n', '<leader>j', ':wincmd j<cr>',{desc = 'Focus Window Down [J]'})
+vim.keymap.set('n', '<leader>k', ':wincmd k<cr>',{desc = 'Focus Window Up [K]'})
+vim.keymap.set('n', '<leader>l', ':wincmd l<cr>',{desc = 'Focus Window Right [L]'})
+vim.keymap.set('n', '<leader>o', ':wincmd o<cr>',{desc = '[O]nly Current Window'})
+vim.keymap.set('n', '<leader>ww', ':wa<cr>',{desc = 'Save All Buffers [W]'})
+
